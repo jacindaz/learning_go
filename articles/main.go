d7467e4c26d96e90a6663f2afb687115eb45fafc
Jacinda Zhong <jacinda.zhong@alto.com>
2021-06-06 14:28:08 -0700

    Add route+page for single article

--- articles/main.go
+++ articles/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
 type Article struct {
+	Id      string `json:"Id"`
 	Title 	string `json:"Title"`
 	Desc 	string `json:"desc"`
 	Content string `json:"content"`
@@ -26,14 +28,15 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/articles/{id}", returnSingleArticle)
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
 	Articles = []Article{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description 2", Content: "Article Content 2"},
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description 2", Content: "Article Content 2"},
 	}
 
 	handleRequests()
@@ -45,4 +48,19 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request){
 	// encodes Articles array into a JSON string
 	// writes as part of response
 	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint Hit: returnSingleArticle")
+
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	index, err := strconv.Atoi(key)
+	if err == nil {
+		article := Articles[index-1]
+		json.NewEncoder(w).Encode(article)
+	} else {
+		fmt.Fprintf(w, "Error, could not process request")
+	}
 }
